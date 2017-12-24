25f4a335a1ae1ed7a9e09c1ff7c61371bcfc5887
dilekusa <dilekuslubeyaz@gmail.com>
2017-12-23 21:23:34 -0500

    test

---
+++ src/arrays/testtt.java
@@ -0,0 +1,26 @@
+package arrays;
+
+public class testtt {
+
+	public static void main(String s[]) {
+		float f=75.0f;
+			double d=75.0;
+			int i=75;
+			if(f==d){
+				if(f==i){
+					System.out.println("f, d, i are equal");
+				}
+				else
+				{
+					System.out.println("f, d equal. not i equal");
+				}
+			}
+			else{
+				System.out.println("f d are not equal");
+			}
+			System.out.println('a'+'b');
+			
+			System.out.println(5/0);
+	}
+
+}
